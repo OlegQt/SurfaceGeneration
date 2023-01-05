f8cb652c6a58bee06f4b68d473bc02ebeb04dbb1
OlegQt <xqjh@mail.ru>
2023-01-05 09:53:48 +0300

    New dev-branch

--- app/src/main/java/com/example/surfacegeneration/MainActivity.java
+++ app/src/main/java/com/example/surfacegeneration/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.surfacegeneration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    protected TextView txtInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtInfo=findViewById(R.id.txtInfo);
+
+        String strInfo = "Surface application \n created successfully";
+        txtInfo.setText(strInfo);
     }
 }
